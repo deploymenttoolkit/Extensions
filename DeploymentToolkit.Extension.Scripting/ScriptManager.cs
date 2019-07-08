4232bc5bc4a6f971af40bac9d924eb4555c90df3
Gérald Döserich <github@doeseri.ch>
2019-07-08 19:55:58 +0200

    Added DeploymentToolkit.Extension.Scripts project

---
+++ DeploymentToolkit.Extension.Scripting/ScriptManager.cs
@@ -0,0 +1,7 @@
+﻿namespace DeploymentToolkit.Extension.Scripting
+{
+    public class ScriptManager
+    {
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+    }
+}
